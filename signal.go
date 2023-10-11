676b6b8cc05ec24f7f91691763fca779329a1290
Your Name Here <you@example.com>
2023-10-11 03:45:46 -0700

    cleaning up

--- signal.go
+++ signal.go
@@ -10,31 +10,12 @@ import (
 // A signal handler function can have any name, but must have return type void and have one int parameter.
 // Example: you might choose the name sigchld_handler for a signal handler for the SIGCHLD signal (termination of a child process). Then the declaration would be:
 //
-//   void sigchld_handler(int sig);
-
-// Signals
-///////////////////////////////////////////////////////////////////////////////
-// REF: runtime/sigtab_linux_generic.go (Go source code)
-// These are what is defined below as the shutdown or exit signal group
-// SIGHUP: terminal line hangup
-// SIGINT: interrupt
-// SIGQUIT: quit (core dump expected)
-// SIGABRT: abort
-// SIGTERM: termination
-
-type SignalType int
-
-const (
-	ShutdownType SignalType = iota
-)
-
-var ShutdownSignal = []os.Signal{
-	syscall.SIGINT,
-	syscall.SIGTERM,
-	syscall.SIGQUIT,
-	syscall.SIGHUP,
-	syscall.SIGKILL,
-}
+//	void sigchld_handler(int sig);
+//type SignalType int
+//
+//const (
+//	ShutdownType SignalType = iota
+//)
 
 var (
 	// NOTE: This allows packages using this signal system to have a signal import
