841c5d651a57ba78c7cf25bd4f43e8330272825f
Your Name Here <you@example.com>
2023-10-11 03:30:07 -0700

    cleaning up a bit on handler so it fits convention better

--- handler.go
+++ handler.go
@@ -44,84 +44,84 @@ func (self Handler) handle(incomingSignal os.Signal) {
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////
-func (self Handler) Add(function func(os.Signal), signals ...os.Signal) Handler {
-	self.mutex.Lock()
-	defer self.mutex.Unlock()
+// /////////////////////////////////////////////////////////////////////////////
+func (h Handler) Add(function func(os.Signal), signals ...os.Signal) Handler {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
 	for _, s := range signals {
-		self.hooks[s] = append(self.hooks[s], function)
-		signal.Notify(self.channel, s)
+		h.hooks[s] = append(h.hooks[s], function)
+		signal.Notify(h.channel, s)
 	}
-	return self
+	return h
 }
 
 // NOTE: Shutdown adds the defined hook to all shutdown/exit signals
-func (self Handler) OnShutdown(function func(os.Signal)) Handler {
-	return self.Add(function, ShutdownSignals...)
+func (h Handler) OnShutdown(function func(os.Signal)) Handler {
+	return h.Add(function, ShutdownSignals...)
 }
 
-func (self Handler) OnInterrupt(function func(os.Signal)) Handler {
-	return self.Add(function, Interrupt)
+func (h Handler) OnInterrupt(function func(os.Signal)) Handler {
+	return h.Add(function, Interrupt)
 }
-func (self Handler) OnTerminate(function func(os.Signal)) Handler {
-	return self.Add(function, Terminate)
+func (h Handler) OnTerminate(function func(os.Signal)) Handler {
+	return h.Add(function, Terminate)
 }
-func (self Handler) OnQuit(function func(os.Signal)) Handler   { return self.Add(function, Quit) }
-func (self Handler) OnHangup(function func(os.Signal)) Handler { return self.Add(function, Hangup) }
-func (self Handler) OnKill(function func(os.Signal)) Handler   { return self.Add(function, Kill) }
+func (h Handler) OnQuit(function func(os.Signal)) Handler   { return h.Add(function, Quit) }
+func (h Handler) OnHangup(function func(os.Signal)) Handler { return h.Add(function, Hangup) }
+func (h Handler) OnKill(function func(os.Signal)) Handler   { return h.Add(function, Kill) }
 
-///////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////
 // TODO: Would be nice to eventually build in functionality to turly ignore
 // signals even force kill signals; possibly via uninterruptable sleep or some
 // similar technique to make this a truly general use signal handling library
-func (self Handler) Ignore(signals ...os.Signal) Handler {
-	self.ignored = append(self.ignored, signals...)
-	for _, ignoredSignal := range self.ignored {
-		self.Remove(ignoredSignal)
-		signal.Ignore(self.Signals()...)
+func (h Handler) Ignore(signals ...os.Signal) Handler {
+	h.ignored = append(h.ignored, signals...)
+	for _, ignoredSignal := range h.ignored {
+		h.Remove(ignoredSignal)
+		signal.Ignore(h.Signals()...)
 	}
-	return self
+	return h
 }
 
 // NOTE: Stop ignoring will stop listening too since it requires use of reset
 // which resets both Notify and Ignore calls
-func (self Handler) StopIgnoring() Handler {
+func (h Handler) StopIgnoring() Handler {
 	signal.Reset()
-	return self
+	return h
 }
 
-func (self Handler) StopListening() Handler {
-	signal.Stop(self.channel)
-	return self
+func (h Handler) StopListening() Handler {
+	signal.Stop(h.channel)
+	return h
 }
 
-///////////////////////////////////////////////////////////////////////////////
-func (self Handler) Remove(s os.Signal) Handler {
-	self.mutex.Lock()
-	defer self.mutex.Unlock()
-	delete(self.hooks, s)
-	return self
+// /////////////////////////////////////////////////////////////////////////////
+func (h Handler) Remove(s os.Signal) Handler {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	delete(h.hooks, s)
+	return h
 }
 
-func (self Handler) Clear() Handler {
-	self.mutex.Lock()
-	defer self.mutex.Unlock()
-	for s := range self.hooks {
-		delete(self.hooks, s)
+func (h Handler) Clear() Handler {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+	for s := range h.hooks {
+		delete(h.hooks, s)
 	}
-	return self
+	return h
 }
 
-func (self Handler) Reset() Handler {
-	self.Clear()
-	self.StopListening()
-	return self
+func (h Handler) Reset() Handler {
+	h.Clear()
+	h.StopListening()
+	return h
 }
 
-///////////////////////////////////////////////////////////////////////////////
-func (self Handler) Signals() []os.Signal {
-	signals := make([]os.Signal, len(self.hooks))
-	for s, _ := range self.hooks {
+// /////////////////////////////////////////////////////////////////////////////
+func (h Handler) Signals() []os.Signal {
+	signals := make([]os.Signal, len(h.hooks))
+	for s, _ := range h.hooks {
 		signals = append(signals, s)
 	}
 	return signals
